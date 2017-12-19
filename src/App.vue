daba6b34ab5a3bc015959acf59e4de21eb6257af
rajat arora <rajat.arora@hotmail.ca>
2017-12-19 01:49:07 +0000

    init commit

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template>
+    <div id="app">
+      <div id="center">
+      <Emoji    :rainAway="rainAway" />
+      
+      <div id = "text">
+      <h1 >  What is this even</h1>
+      
+      <p >This is a <a href="https://vuejs.org/">Vue.js</a> element that you can drop on any web page and make it rain emoji. Because, why in the world wouldn't you?</p>
+      <button v-on:click="rain()"> Make it rain!!! <img width="50px" src="./assets/dolla.png"></img> </button>
+      <hr/>
+       <div id="footer">
+      <p>Made with <span style="color: #E91E63">❤︎</span> by <a href="https://twitter.com/rajatonit">rajat</a>.</p>
+    </div>
+
+      </div>
+      </div>
+
+
+    </div>
+</template>
+<style>
+  #text{
+  text-align:center;
+   position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  }
+  h1, button{
+        font-family: 'Lato', sans-serif;
+      font-weight: 300;
+    font-size: 2em;
+  }
+  body{
+      color: #535368;
+      font-family: Helvetica, Arial, sans-serif;
+      line-height: 1.5;
+      font-weight: 300;
+  }
+  a:link, a:visited, button {
+    color: #E91E63;
+    text-decoration: none;
+    padding-bottom: 5px;
+    padding-top: 5px;
+    background: none;
+    border: none;
+    box-shadow: none;
+    outline: none;
+}
+hr{
+    border-top: 2px solid #E91E63;
+
+}
+
+button:hover{
+    color:white;
+    background-color:#fc005a
+}
+ 
+</style>
+
+<script>
+  import Emoji from './Emoji-Rain.vue'
+  
+    export default{
+        data(){
+          return{
+            rainAway:false
+          }  
+        },
+        methods: {
+          rain(){
+           if(this.rainAway){
+            this.rainAway = false;
+          }else{
+           this. rainAway = true;
+          }
+          }
+        },
+         components:{
+            Emoji       
+        }
+    }
+</script>
